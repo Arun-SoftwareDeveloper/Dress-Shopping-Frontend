e776e9ea47bb7f4157559b21d08f4c95becb8795
Arun-SoftwareDeveloper <arunramasamy46@gmail.com>
2023-10-07 11:48:40 +0530

    modified:   src/component/Cart.js 	modified:   src/component/Footer.js 	modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 // import Cart from "./component/Cart"
-import Directory from "./component/Directory";
-import AddToCart from "./component/AddToCart";
+// import Directory from "./component/Directory";
+// import AddToCart from "./component/AddToCart";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
